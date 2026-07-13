594bee9215027da5050f68dc5a96baf385e45781
Khaing <khaing12boy12@gmail.com>
2026-07-13 14:54:30 +0900

    Projectにmigration を作成するコメント

--- Project_command.sql
+++ Project_command.sql
@@ -2,3 +2,7 @@ Add-Migration InitialCreate
 Update-Database
 
 git pull origin developer --allow-unrelated-histories
+
+--Git lab に切り替えるコメント
+git remote set-url origin 【コピーしたGitLabのURL】
+git push origin developer
