8d870503d3d7ef85c726efc159c48759a7407689
Khaing <khaing12boy12@gmail.com>
2026-07-15 09:53:20 +0900

    追加ファイル

---
+++ Practise_project/Controllers/Project_command.sql
@@ -0,0 +1,8 @@
+dotnet ef migrations add AddAgeToPerson
+dotnet ef database update
+
+git pull origin developer --allow-unrelated-histories
+
+--Git lab に切り替えるコメント
+git remote set-url origin 【コピーしたGitLabのURL】
+git push origin developer
