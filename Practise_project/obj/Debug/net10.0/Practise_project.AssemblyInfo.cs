b85734a907aa4072b2d51b127b44358405dcaedd
Khaing <khaing12boy12@gmail.com>
2026-07-22 12:04:37 +0900

    一部の修正

--- Practise_project/obj/Debug/net10.0/Practise_project.AssemblyInfo.cs
+++ Practise_project/obj/Debug/net10.0/Practise_project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practise_project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f67aed231ba2ee188510ab407a3eda9aaabceaf5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63ae4c326a50852b79b5134dab718d37ccc76e79")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practise_project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practise_project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
