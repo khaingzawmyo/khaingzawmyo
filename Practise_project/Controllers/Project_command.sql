040087d6f98cedf7e34aa4f33ecf0ec171ea765a
khaingzawmyo <110839543+khaingzawmyo@users.noreply.github.com>
2026-07-23 10:41:23 +0900

    Delete Practise_project directory

--- Practise_project/Controllers/Project_command.sql
+++
@@ -1,8 +0,0 @@
-Add-Migration InitialCreate
-Update-Database
-
-git pull origin developer --allow-unrelated-histories
-
---Git lab に切り替えるコメント
-git remote set-url origin 【コピーしたGitLabのURL】
-git push origin developer
